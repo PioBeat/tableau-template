67d91290b122674c120361a6f8d50752f46b1a41
Dominik Grzelak <dominik@offbeat-pioneer.net>
2017-06-25 18:32:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# TileIt - Web template
+**by OffbeatPioneer**
+http://www.offbeat-pioneer.net
+
+Free for personal and commercial use under the CCA 3.0 license (License_template.txt)
+
+## Description
+A slick one-page design where each section is represented as a tile.
+
+Use CSS class ``.tile-row-2`` or ``.tile-row-3`` to display two or three
+rows with tiles. The number of tiles in each row can be specified with the bootstrap
+classes ``.col-md-*`` / ``.col-lg-*``. For the best layout you should arrange the tiles 
+2x2, 2x3, or 3x3.
+
+In each ``.tile`` column of every row you can do the following:<br/>
+Reference the section id with the data attribute ``data-target-id``, and specify the open animation 
+for each section with the data attribute ``data-open-animation``.
+
+Includes a custom footer panel at the bottom of the side to show a contact
+formular or something else. For small screens the footer panel is displayed at
+the top of the page so that the user can access it at any time when viewing the 
+content.
+
+## Tested in:
+
+FF3.5+
+
+
+## Credits:
+
+- Libraries
+    * jQuery 1.12.0
+    * modernizr 2.8.3
+    * HTML5 Boilerplate
+    * Bootstrap 3.3.7
+    * CSS3 animations with [http://animista.net](http://animista.net)
+    
+- Web fonts
+    * Josefin Sans [Google fonts](https://fonts.googleapis.com/css?family=Josefin+Sans)
+    
+- Icons:
+    * font awesome 4.7.0
+    * "Education elements" from [Flaticon](http://www.flaticon.com/packs/education-elements-3)
+
+	
---
+++ js/tile-template.js
@@ -0,0 +1,45 @@
+$(function () {
+
+    function scrollWindowTop() {
+//            window.scrollTo(0, 0);
+        $("html, body").animate({scrollTop: 0}, "fast");
+    }
+
+    $(".tile").on("mouseenter", function () {
+        $(this).find(".inner").addClass("scale-in-center");
+        $(this).find("h1").addClass("text-shadow-pop-bottom").css("z-index", "99999");
+    }).on("mouseleave", function () {
+        $(this).find(".inner").removeClass("scale-in-center");
+        $(this).find("h1").removeClass("text-shadow-pop-bottom");
+    }).on("click", function () {
+        var targetId = $(this).data("target-id");
+        var anim = $(this).data("open-animation");
+        $(targetId).removeClass("hidden").addClass(anim).data("open-animation", anim);
+        $(".container-tiles").removeClass("text-blur-out-reverse").addClass("text-blur-out");
+        setTimeout(function (e) {
+            $(".container-tiles").addClass("hidden");
+        }, 1000);
+        scrollWindowTop();
+    });
+
+    $(".row-about a").on("click", function () {
+        var $footer = $("footer");
+        $footer.removeClass("hidden slide-in-top slide-in-bottom");
+        if (screen.width <= 992) {
+            $footer.removeClass("hidden").addClass("slide-in-top");
+        } else {
+            $footer.removeClass("hidden").addClass("slide-in-bottom");
+        }
+    });
+
+    $("footer .closer").on("click", function () {
+        $("footer").addClass("hidden");
+    });
+
+    $(".header > a").on("click", function () {
+        var anim = $(this).closest("section").data("open-animation");
+        $(this).closest("section").addClass("hidden").removeClass("slide " + anim);
+        $(".container-tiles").removeClass("hidden text-blur-out").addClass("text-blur-out-reverse");
+    });
+
+});
